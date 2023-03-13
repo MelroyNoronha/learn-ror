aba7cb5178c42cffc82d63ca47c935f2034cf459
Melroy Noronha <melmax1208@gmail.com>
2023-03-13 18:57:15 +0530

    Setup tailwindcss

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_02_24_081504) do
   create_table "tasks", force: :cascade do |t|
-    t.text "body"
+    t.text "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
